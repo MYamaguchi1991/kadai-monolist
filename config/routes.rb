3a0ccfa273d30d52eda62d6c78ee5d8d338aed07
masayuki yamaguchi <ymgc.msyk31@gmail.com>
2017-04-24 02:24:52 +0000

    want

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :new, :create]
 
   resources :items, only: [:new]
+  resources :ownerships, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
